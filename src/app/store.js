9e04ebcedf52e7598ba9a698aa7419a6fdd68139
Samet <sametozturk973@gmail.com>
2024-05-27 14:47:09 +0300

    Template

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
-import headerSlice from '../features/common/headerSlice'
-import modalSlice from '../features/common/modalSlice'
-import rightDrawerSlice from '../features/common/rightDrawerSlice'
-import leadsSlice from '../features/leads/leadSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import headerSlice from "../features/common/headerSlice";
+import modalSlice from "../features/common/modalSlice";
+import rightDrawerSlice from "../features/common/rightDrawerSlice";
 
 const combinedReducer = {
-  header : headerSlice,
-  rightDrawer : rightDrawerSlice,
-  modal : modalSlice,
-  lead : leadsSlice
-}
+  header: headerSlice,
+  rightDrawer: rightDrawerSlice,
+  modal: modalSlice,
+};
 
 export default configureStore({
-    reducer: combinedReducer
-})
+  reducer: combinedReducer,
+});
