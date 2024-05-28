c8aee5ae163c7d35ad429a2fc655870bf196130a
Mgokayay <aymustafagokay@gmail.com>
2024-05-28 18:59:54 +0300

    patient edited

--- src/features/patient/patient.js
+++ src/features/patient/patient.js
@@ -21,39 +21,44 @@ const Patient = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label>Name</label>
+      <div className="w-[200px] py-2">
+        <label>Full Name</label>
         <input
           id="fullName"
           {...register("fullName", { required: "this field is required" })}
         />
         {errors.fullName && <p>{errors.fullName.message}</p>}
       </div>
-      <div>
-        <label>Name</label>
+      <div className="w-[200px] py-2">
+        <label>Date</label>
         <input
           id="date"
           {...register("date", { required: "this field is required" })}
         />
         {errors.date && <p>{errors.date.message}</p>}
       </div>
-      <div>
-        <label>Name</label>
+      <div className="w-[200px] py-2">
+        <label>Gender</label>
         <input
           id="gender"
           {...register("gender", { required: "this field is required" })}
         />
         {errors.gender && <p>{errors.gender.message}</p>}
       </div>
-      <div>
-        <label>Name</label>
+      <div className="w-[200px] py-2">
+        <label>TCKN</label>
         <input
           id="tckn"
           {...register("tckn", { required: "this field is required" })}
         />
         {errors.tckn && <p>{errors.tckn.message}</p>}
       </div>
-      <button type="submit">save</button>
+      <button
+        className="bg-black hover:bg-green-700 hover:scale-105 border mt-4 p-2"
+        type="submit"
+      >
+        Save
+      </button>
     </form>
   );
 };
